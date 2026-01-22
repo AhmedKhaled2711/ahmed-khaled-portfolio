0eacf815f857c2e0f1d22675ea18824f1b8217bb
Ahmed Khaled <154894528+AhmedKhaled2711@users.noreply.github.com>
2026-01-22 13:20:13 -0800

    Update homepage URL, adjust base href in index.html, and remove unused 404 redirect page; refactor App component to remove Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { HashRouter as Router } from "react-router-dom";
 import "./App.scss";
 import Main from "./containers/Main";
 
 function App() {
   return (
-    <Router basename="/">
-      <div className="App">
-        <Main />
-      </div>
-    </Router>
+    <div>
+      <Main />
+    </div>
   );
 }
 
